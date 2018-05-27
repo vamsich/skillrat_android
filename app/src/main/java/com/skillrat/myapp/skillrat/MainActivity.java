e5433afb0001acc7979c95064fde8fbbeccbc12d
vamsi <tovamsich@gmail.com>
2018-05-28 03:14:33 +0530

    initial code

--- app/src/main/java/com/skillrat/myapp/skillrat/MainActivity.java
+++ app/src/main/java/com/skillrat/myapp/skillrat/MainActivity.java
@@ -1,14 +1,69 @@
 package com.skillrat.myapp.skillrat;
 
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import com.skillrat.myapp.fragments.HomeFragment;
+
+import static android.R.attr.fragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
 {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            loadFragment(new HomeFragment());
+
+            //getting bottom navigation view and attaching the listener
+            BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.navigation);
+            navigation.setOnNavigationItemSelectedListener(this);
+        }
+
+        private boolean loadFragment(Fragment fragment)
+        {
+            if(fragment !=null)
+            {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment).commit();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            Fragment fragment= null;
+
+            switch (item.getItemId())
+            {
+                case R.id.navigation_home:
+                    fragment= new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment= new HomeFragment();
+                    break;
+                case R.id.navigation_sessions:
+                    fragment= new HomeFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment= new HomeFragment();
+                    break;
+                case R.id.navigation_profile:
+                    fragment= new HomeFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
     }
-}
