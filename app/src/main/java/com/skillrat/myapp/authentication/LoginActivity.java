f20807c9cc7be8f5bd9b55b19e5e01b3961f03fd
Suneelkumar jami <suneel.j@dhanushinfotech.net>
2018-06-04 23:36:46 +0530

    app intro

--- app/src/main/java/com/skillrat/myapp/authentication/LoginActivity.java
+++ app/src/main/java/com/skillrat/myapp/authentication/LoginActivity.java
@@ -8,59 +8,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 
+import com.skillrat.myapp.database.PrefManager;
+import com.skillrat.myapp.skillrat.AppIntroActivity;
 import com.skillrat.myapp.skillrat.MainActivity;
 import com.skillrat.myapp.skillrat.R;
+
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * Created by Arun1234 on 5/23/2018.
  */
 
-public class LoginActivity extends Activity
-{
+public class LoginActivity extends Activity {
     private static final String TAG = "LoginActivity";
-
+    PrefManager prefManager;
     private static final int REQUEST_SIGNUP = 0;
+Button btn_login;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        final Context context= this;
+        final Context context = this;
         setContentView(R.layout.activity_login);
-
-        TextView forgotPwdText=(TextView)findViewById(R.id.link_frgtpwd);
-        TextView signupText=(TextView)findViewById(R.id.link_signup);
-        TextView skipText=(TextView)findViewById(R.id.skip);
-        forgotPwdText.setOnClickListener(new View.OnClickListener()
-        {
+        prefManager = new PrefManager(LoginActivity.this);
+        TextView forgotPwdText = (TextView) findViewById(R.id.link_frgtpwd);
+        TextView signupText = (TextView) findViewById(R.id.link_signup);
+        TextView skipText = (TextView) findViewById(R.id.skip);
+        btn_login= (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent intent= new Intent(context, ForgotPasswordActivity.class);
+            public void onClick(View view) {
+
+                if (prefManager.isFirstTimeLaunch()) {
+
+                    Intent intent = new Intent(context, AppIntroActivity.class);
+                    startActivity(intent);
+                } else {
+
+                    Intent intent = new Intent(context, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        forgotPwdText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(context, ForgotPasswordActivity.class);
                 startActivity(intent);
             }
         });
 
-        signupText.setOnClickListener(new View.OnClickListener()
-        {
+        signupText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent intent= new Intent(context, RegistrationActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(context, RegistrationActivity.class);
                 startActivity(intent);
             }
         });
 
-        skipText.setOnClickListener(new View.OnClickListener()
-        {
+        skipText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent intent= new Intent(context, MainActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(context, AppIntroActivity.class);
                 startActivity(intent);
             }
         });
