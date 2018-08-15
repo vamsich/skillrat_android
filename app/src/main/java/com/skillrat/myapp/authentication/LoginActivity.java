8dbd196acb1e812fdf9d78973d4c6ddb4a9d19e7
Suneelkumar jami <suneel.j@dhanushinfotech.net>
2018-08-15 17:00:07 +0530

    app intro

--- app/src/main/java/com/skillrat/myapp/authentication/LoginActivity.java
+++ app/src/main/java/com/skillrat/myapp/authentication/LoginActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.skillrat.myapp.database.PrefManager;
 import com.skillrat.myapp.skillrat.AppIntroActivity;
 import com.skillrat.myapp.skillrat.MainActivity;
@@ -26,6 +32,8 @@ public class LoginActivity extends Activity {
     PrefManager prefManager;
     private static final int REQUEST_SIGNUP = 0;
 Button btn_login;
+    private LoginButton fbLoginButton;
+    private CallbackManager callbackManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,12 +41,16 @@ Button btn_login;
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         final Context context = this;
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
         prefManager = new PrefManager(LoginActivity.this);
         TextView forgotPwdText = (TextView) findViewById(R.id.link_frgtpwd);
         TextView signupText = (TextView) findViewById(R.id.link_signup);
         TextView skipText = (TextView) findViewById(R.id.skip);
         btn_login= (Button) findViewById(R.id.btn_login);
+        fbLoginButton = (LoginButton)findViewById(R.id.login_button);
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,6 +88,27 @@ Button btn_login;
             public void onClick(View view) {
                 Intent intent = new Intent(context, AppIntroActivity.class);
                 startActivity(intent);
+            }
+        });
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("fb loginsuccess","loginresult"+ "User ID: "
+                        + loginResult.getAccessToken().getUserId()
+                        + "\n" +
+                        "Auth Token: "
+                        + loginResult.getAccessToken().getToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
             }
         });
     }
